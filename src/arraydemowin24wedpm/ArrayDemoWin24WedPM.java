f318852e09521ad62c4548f587bfd0ef04e9c6e8
sanjana <sanjana5@sheridancollege.ca>
2024-01-24 13:48:54 -0500

    first commit

---
+++ src/arraydemowin24wedpm/ArrayDemoWin24WedPM.java
@@ -0,0 +1,17 @@
+
+package arraydemowin24wedpm;
+
+/**
+ *
+ * @author sanju
+ */
+public class ArrayDemoWin24WedPM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
