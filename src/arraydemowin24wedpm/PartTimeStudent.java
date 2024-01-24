5a73260b0d569c7a18ad2b54a61000d57ae47b77
sanjana <sanjana5@sheridancollege.ca>
2024-01-24 14:37:55 -0500

    Added class

---
+++ src/arraydemowin24wedpm/PartTimeStudent.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemowin24wedpm;
+
+/**
+ *
+ * @author sanju
+ */
+public class PartTimeStudent  extends student{
+      
+    private int numOfCourses;
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+  
+    
+    
+}
