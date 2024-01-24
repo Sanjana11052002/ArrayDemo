cb9e6e41451d02878e1fc5726f7b4807b99cdbb7
sanjana <sanjana5@sheridancollege.ca>
2024-01-24 13:55:04 -0500

    added commit

--- src/arraydemowin24wedpm/student.java
+++ src/arraydemowin24wedpm/student.java
@@ -1,11 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+ 
 package arraydemowin24wedpm;
 
 /**
- *
+ * added few comment on 24 jan at 1:53PM
  * @author sanju
  */
 public class student {
